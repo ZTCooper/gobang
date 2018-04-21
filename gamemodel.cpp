a3cf6e11e27f7a920daeac8467cce3252d0d6ebd
ztcooper <ztcooper@outlook.com>
2018-04-21 15:06:40 +0800

    判断输赢

--- gamemodel.cpp
+++ gamemodel.cpp
@@ -5,6 +5,7 @@ GameModel::GameModel()
 
 }
 
+//游戏开始，，初始化棋盘格和评分表
 void GameModel :: startGame(GameType type){
     gameType = type;
 
@@ -32,3 +33,61 @@ void GameModel :: startGame(GameType type){
     //己方为1，对方为0
     playerFlag = true;
 }
+
+//判断输赢
+bool GameModel :: isWin(int row, int col){
+    //水平方向
+    for(int i = 0; i < 5; i++){
+        if(col - i > 0 &&
+           col - i + 4 < BoardSize &&
+           gameMapVec[row][col - i] == gameMapVec[row][col - i + 1] &&
+           gameMapVec[row][col - i] == gameMapVec[row][col - i + 2] &&
+           gameMapVec[row][col - i] == gameMapVec[row][col - i + 3] &&
+           gameMapVec[row][col - i] == gameMapVec[row][col - i + 4])
+            return true;
+    }
+
+    //竖直方向
+    for(int i = 0; i < 5; i++){
+        if(row - i > 0 &&
+           row - i + 4 < BoardSize &&
+           gameMapVec[row - i][col] == gameMapVec[row - i + 1][col] &&
+           gameMapVec[row - i][col] == gameMapVec[row - i + 2][col] &&
+           gameMapVec[row - i][col] == gameMapVec[row - i + 3][col] &&
+           gameMapVec[row - i][col] == gameMapVec[row - i + 4][col])
+            return true;
+    }
+
+    //主对角线
+    for(int i = 0; i < 5; i++){
+        if(row - i > 0 &&
+           row - i + 4 < BoardSize &&
+           col - i > 0 &&
+           col - i + 4 < BoardSize &&
+           gameMapVec[row - i][col - i] == gameMapVec[row - i + 1][col - i + 1] &&
+           gameMapVec[row - i][col - i] == gameMapVec[row - i + 2][col - i + 2] &&
+           gameMapVec[row - i][col - i] == gameMapVec[row - i + 3][col - i + 3] &&
+           gameMapVec[row - i][col - i] == gameMapVec[row - i + 4][col - i + 4])
+            return true;
+    }
+
+    //副对角线
+    for(int i = 0; i < 5; i++){
+        if(row - i < BoardSize &&
+           row - i + 4 > 0 &&
+           col - i > 0 &&
+           col - i + 4 < BoardSize &&
+           gameMapVec[row - i][col + i] == gameMapVec[row - i + 1][col + i - 1] &&
+           gameMapVec[row - i][col + i] == gameMapVec[row - i + 2][col + i - 2] &&
+           gameMapVec[row - i][col + i] == gameMapVec[row - i + 3][col + i - 3] &&
+           gameMapVec[row - i][col + i] == gameMapVec[row - i + 4][col + i - 4])
+            return true;
+    }
+
+    return false;
+}
+
+
+
+
+
