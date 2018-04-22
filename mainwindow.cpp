ac080352b499dd7450f7d579ebab6c4eb6e35438
ztcooper <ztcooper@outlook.com>
2018-04-22 22:49:18 +0800

    initialize game

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,15 +14,22 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::initGame(){
-
+    game = new GameModel;
+    initPVPGame();
 }
 
 void MainWindow::initPVPGame(){
-
+    game_type = P2P;
+    game->gameStatus = PLAYING;
+    game->startGame(game_type);
+    update();
 }
 
 void MainWindow::initPVBGame(){
-
+    game_type = BOT;
+    game->gameStatus = PLAYING;
+    game->startGame(game_type);
+    update();
 }
 
 void MainWindow::mouseMoveEvent(QMouseEvent *event){
